ff18ec2314a48427a554e40d3bb044ed153707e5
Nafis Anjoom <nafisanjoom@gmail.com>
2020-01-19 11:00:21 -0800

    "Beta Build"

--- src/Testbench.java
+++ src/Testbench.java
@@ -1,42 +1,49 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.io.IOException;
 
-public class Run
+
+public class Testbench
 {
-    private String link;
-    public static Run[] data = new Run[10];
 
-    public Run(String... str) throws IOException
+    public static void main(String[] args) throws IOException
     {
-        this(str[0],str[1], str[2], str[3]);
-    }
 
-
-    public  Run(String course, String section, String dept, String email) throws IOException
-    {
-        //https://courses.students.ubc.ca/cs/courseschedule?campuscd=UBCO&pname=subjarea&tname=subj-section&course=121&section=L09&dept=PHYS
-        link = "https://courses.students.ubc.ca/cs/courseschedule?campuscd=UBCO&pname=subjarea&tname=subj-section&course=" + course + "&section=" + section + "&dept=" + dept;
         Timer timer = new Timer();
         SendEmail mail = new SendEmail();
         TimerTask task = new TimerTask()
+
         {
             @Override
             public void run()
             {
+
+                //Demo Purposes
+
                 Document doc = null;
+                String file = "";
                 try
                 {
-                    doc = Jsoup.connect(link).get();
+                    file = Files.readString(Paths.get("C:\\Users\\nafis\\IdeaProjects\\BCHacks2020\\sample.html"));
                 }
-                catch (IOException a)
+                catch(IOException e)
                 {
 
                 }
+
+
+
+
+                doc = Jsoup.parse(file);
+                //System.out.println(doc.toString());
+                //doc = Jsoup.connect("https://courses.students.ubc.ca/cs/courseschedule?campuscd=UBCO&pname=subjarea&tname=subj-section&course=121&section=L09&dept=PHYS").get();
+
 
                 Element table = doc.getElementsByClass("'table").first();
                 List<String> seatSummary = table.getElementsByTag("td").eachText();
@@ -46,8 +53,7 @@ public class Run
                     timer.cancel();
                     timer.purge();
                     System.out.println("The class has freed up.");
-                    String courseId = dept + " " + course + " Section " + section;
-                    mail.sendFromGmail("sscalertsystem@gmail.com", "bchacks2020", new String[]{email}, courseId, courseId + " course has freed up.");
+                    mail.sendFromGmail("sscalertsystem@gmail.com", "bchacks2020", new String[]{"lakshaykarnwal@gmail.com", "nafisanjoom@gmail.com", "zack.hamza2001@gmail.com"}, "PHYS 101 L09", "This Class has freed up.");
                 }
             }
         };
